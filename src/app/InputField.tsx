c8d36dd5707196aaa467b7e18cb3e86c7b3ab2e3
Brandon Garcia <grandonleebarcia@gmail.com>
2023-09-08 15:26:17 -0700

    implement server side component

---
+++ src/app/InputField.tsx
@@ -0,0 +1,3 @@
+export default function Inputfield() {
+  return <div></div>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,9 @@
-'use client';
-
-import Image from 'next/image';
 import axios from 'axios';
-import { useState } from 'react';
 
-export default function Home() {
+export default async function Home() {
   const url = `http://api.openweathermap.org/geo/1.0/direct?q=dubai&appid=${process.env.NEXT_PUBLIC_API_KEY}`;
+  const data = await axios.get(url);
+  console.log(data);
 
-  const [city, setCity] = useState('');
-  const [weather, setWeather] = useState({});
-  const [loading, setLoading] = useState(false);
-
-  async function fetchWeather() {
-    setLoading(true);
-    try {
-      const data = await axios.get(url);
-      console.log(data.data);
-    } catch (error) {}
-  }
-  return (
-    <main>
-      <button onClick={fetchWeather}>Click Me!</button>
-    </main>
-  );
+  return <main></main>;
 }
